e754823cdc5de59a3ec3bcb50848ea03681d10d2
Arindam Sahoo <arindams2002@gmail.com>
2023-08-03 00:56:27 +0530

    Day 2 Done

--- datapilot/datapilot/urls.py
+++ datapilot/datapilot/urls.py
@@ -5,6 +5,7 @@ from datapilot_app.views import home, database_assignment, logout_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('accounts/profile/', database_assignment, name='database_assignment'),
     path('', home, name='home'),
     path('database_assignment/', database_assignment, name='database_assignment'),
     path('logout/', logout_view, name='logout'),
--- datapilot/datapilot_app/views.py
+++ datapilot/datapilot_app/views.py
@@ -1,22 +1,50 @@
+import os
+import sqlite3
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.db import connection
 from django.contrib.auth import logout
+from django.conf import settings
 from .utils import assign_database
 
+# def home(request):
+#     if request.method == 'POST':
+#         if request.user.is_authenticated:
+#             sql_query = request.POST['sql_query']
+#             with connection.cursor() as cursor:
+#                 try:
+#                     cursor.execute(sql_query)
+#                     rows = cursor.fetchall()
+#                 except Exception as e:
+#                     error_message = str(e)
+#                     return render(request, 'datapilot_app/home.html', {'error_message': error_message})
+
+#             return render(request, 'datapilot_app/home.html', {'rows': rows, 'sql_query': sql_query})
+#         else:
+#             return redirect('account_login')
+
+#     return render(request, 'datapilot_app/home.html')
+
+@login_required
 def home(request):
     if request.method == 'POST':
         if request.user.is_authenticated:
             sql_query = request.POST['sql_query']
-            with connection.cursor() as cursor:
-                try:
-                    cursor.execute(sql_query)
-                    rows = cursor.fetchall()
-                except Exception as e:
-                    error_message = str(e)
-                    return render(request, 'datapilot_app/home.html', {'error_message': error_message})
+            database_name = assign_database(request.user)
+            database_path = os.path.join(settings.BASE_DIR, "user_databases", database_name)
 
+            try:
+                conn = sqlite3.connect(database_path)
+                cursor = conn.cursor()
+                cursor.execute(sql_query)
+                rows = cursor.fetchall()
+            except sqlite3.Error as e:
+                error_message = str(e)
+                return render(request, 'datapilot_app/home.html', {'error_message': error_message})
+
+            conn.close()
             return render(request, 'datapilot_app/home.html', {'rows': rows, 'sql_query': sql_query})
+
         else:
             return redirect('account_login')
 
