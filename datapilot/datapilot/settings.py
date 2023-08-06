116724af707bbcd3a8fcdf4916d8338ed1d7745e
Arindam Sahoo <arindams2002@gmail.com>
2023-08-06 20:21:07 +0530

    Production Ready

--- datapilot/datapilot/settings.py
+++ datapilot/datapilot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hi)bvw8q96f+ye$a=ka9k#^(^4o@!#+(8vulh+nwugj%l(x@^p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
